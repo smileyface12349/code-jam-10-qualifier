bda0bf12e06e621ec37dc1b6af4df816916dc5f1
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2023-08-19 20:08:01 +0100

    Did it

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]) -> bool:
     """
     Return True if the given input allows the rearrangement of the image, False otherwise.
@@ -5,6 +7,11 @@ def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], orderin
     The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
     once.
     """
+
+    return image_size[0] % tile_size[0] == 0 \
+       and image_size[1] % tile_size[1] == 0 \
+       and len(ordering) == len(set(ordering)) \
+       and all(0 <= i < ((image_size[0]/tile_size[0]) * (image_size[1]/tile_size[1])) for i in ordering)
 
 
 def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str) -> None:
@@ -18,3 +25,31 @@ def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[
     once. If these conditions do not hold, raise a ValueError with the message:
     "The tile size or ordering are not valid for the given image".
     """
+
+    # open the image using pillow
+    image = Image.open(image_path)
+
+    # check if the input is valid
+    if not valid_input(image.size, tile_size, ordering):
+        raise ValueError("The tile size or ordering are not valid for the given image")
+
+    # split into tiles
+    tiles = []
+    for y in range(0, image.size[1], tile_size[1]):
+        for x in range(0, image.size[0], tile_size[0]):
+            tiles.append(image.crop((x, y, x + tile_size[0], y + tile_size[1])))
+
+    # combine into final image
+    im: Image = Image.new("RGB", image.size)
+    width = 0
+    height = 0
+    for i in ordering:
+        im.paste(tiles[i], (width, height))
+        width += tile_size[0]
+        if width >= image.size[0]:
+            width = 0
+            height += tile_size[1]
+
+    # save the image
+    im.save(out_path)
+
